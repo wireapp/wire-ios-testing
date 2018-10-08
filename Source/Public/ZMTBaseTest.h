b4fd2253d8efbf955043902deca14d735e4b0fab
Jacob Persson <jacob.persson@gmail.com>
2018-10-08 18:07:18 +0200

    Expose largeJPEGData as instance method

--- Source/Public/ZMTBaseTest.h
+++ Source/Public/ZMTBaseTest.h
@@ -83,6 +83,8 @@ typedef BOOL(^VerificationBlock)(void);
 /// Returns the data of a medium JPEG image
 + (nonnull NSData *)mediumJPEGData;
 /// Returns the data of a large JPEG image
+- (nonnull NSData *)largeJPEGData;
+/// Returns the data of a large JPEG image
 + (nonnull NSData *)largeJPEGData;
 
 @property (nonatomic, strong, nonnull, readonly) id<ZMSGroupQueue> fakeUIContext;
